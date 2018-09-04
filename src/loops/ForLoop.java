6ca9f2ff4e977ca22ddfacc6ddad3ad340cef4b6
AlifLailaNabila <aliflailanabila@gmail.com>
2018-09-04 14:28:28 -0400

    Trying

--- src/loops/ForLoop.java
+++ src/loops/ForLoop.java
@@ -1,8 +1,12 @@
 package loops;
 
+import java.util.Random;
+
 public class ForLoop {
     public static void main(String[] args) {
+        Random rd = new Random();
         for(int i =10; i>0; i=i-2){
+            i = 1+rd.nextInt(20);
             System.out.println(i);
         }
         for(int i =0; i<10; i++){
