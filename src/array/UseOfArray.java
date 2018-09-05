99abbf295a9f8459e534c4bfc591f6f0bc3f30b9
AlifLailaNabila <aliflailanabila@gmail.com>
2018-09-05 18:53:30 -0400

    MyHomework

--- src/array/UseOfArray.java
+++ src/array/UseOfArray.java
@@ -1,10 +1,20 @@
 package array;
 
+import java.util.Scanner;
+
 public class UseOfArray {
     public static void main(String[] args) {
+       // Scanner sc = new Scanner(System.in);
         int [] abc = new int [10];
-        abc [0]=4;
-        abc [9]=14;
-        System.out.println(abc[9]);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter numbers: ");
+        for(int i=0;i<abc.length;i++){
+            abc[i]=sc.nextInt();
+        }
+
+        for(int j=0;j<abc.length;j++){
+            System.out.println("Array index value is " +abc[j]);
+        }
+
     }
 }
