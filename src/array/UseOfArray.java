1bacbc0a1dbbc968e2c1fe0d63d4d34db2389417
AlifLailaNabila <aliflailanabila@gmail.com>
2018-09-07 21:17:56 -0400

    Finally

--- src/array/UseOfArray.java
+++ src/array/UseOfArray.java
@@ -1,15 +1,17 @@
 package array;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class UseOfArray {
     public static void main(String[] args) {
-       // Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         int [] abc = new int [10];
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
+        Random random = new Random();
         System.out.println("Please enter numbers: ");
         for(int i=0;i<abc.length;i++){
-            abc[i]=sc.nextInt();
+            abc[i]=random.nextInt(100);
         }
 
         for(int j=0;j<abc.length;j++){
